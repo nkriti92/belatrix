bbfacb441820993740c41d7b2649379e247ac2bd
nkriti92 <nkriti92@gmail.com>
2017-09-28 00:13:30 -0400

    font changes and accordian component

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ $(window).scroll(function(){
     $('.titlebar').removeClass('titlebarshadow');
   }
 });
+$(document).ready(function() {
+	$('.thumbnail').click(function(e) {
+		if($(this).hasClass('col-4')) {
+      // $('.expandable-box.out').not($(this)).removeClass('out');
+      // $(this).removeClass('col-4');
+      $(this).find('.thumbnailbutton').addClass('border');
+    }
+    else {
+      // $(this).addClass('col-4');
+      $(this).find('.thumbnailbutton').removeClass('border');
+    }
+	});
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
